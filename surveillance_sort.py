10ddb398b53efe86ebd52634747c907568893e3b
tobiasisaven <tobiasisave@gmail.com>
2026-06-17 18:04:59 -0400

    Update surveillance_sort.py
    Can be run in command line by naming the raw file 'to_sort'

--- surveillance_sort.py
+++ surveillance_sort.py
@@ -1,30 +1,37 @@
 
-####### 2/12/26 TIN ###################
+####### 6/17/26 Tobias N ###################
 """
-1. Write the name of the folder within the external hard drive that contains unsorted video files here:
-""" 
+1. Put this python file (surveillance_sort.py) in the auto-generated folder called "N863A6" (or change the 'dir' path below)
 
-to_sort = "/2026-2-2" 
-
-
-"""
-2. Put this python file (surveillance_sort.py) one level above that folder, 
-probably in the auto-generated folder called "N863A6" 
+2. Rename the folder of raw videos you want to sort "to_sort"
 
 3. Run this file; it should make a new folder called "output" that contains
 folders for each date and automatically move videos into those folders.
 
 """
+
+to_sort = "/to_sort" 
+dir = "d:/N863A6"
+
+
 #######################################
 
 import shutil
 import os
 
+
+# FOR DEBUGGING
+# dir =  os.path.dirname(os.path.realpath(__name__))
+
+
 # Relative path of output folder
 output = "/output"
 
+
 # Go through all of the unsorted files 
-for filename in os.listdir(os.getcwd()+to_sort):
+for filename in os.listdir(dir+to_sort):
+    print(dir+to_sort+"/"+filename)
+
     # Skip files not formatted like a surveillance video
     if not filename.startswith("N863A6_"):
         continue
@@ -33,12 +40,24 @@ for filename in os.listdir(os.getcwd()+to_sort):
     date = filename.split("_")[3][:8]
     # Reformat date to YYYY-MM-DD
     date_reformat = date[0:4] + "-" + date[4:6] + "-" + date[6:8]
-    newpath = os.getcwd()+output+"/"+date_reformat
+    newpath = dir+output+"/"+date_reformat
+    print(newpath)
 
     # Create a new folder for the date if it doesn't exist
     if not os.path.exists(newpath):
         os.makedirs(newpath)
+        print("Created directory: " + newpath)
 
     # Move the file to the new folder & print the new path of the file
-    print(shutil.move(os.getcwd()+to_sort+"/"+filename, newpath+"/"+filename))
+    print(shutil.move(dir+to_sort+"/"+filename, newpath+"/"+filename))
 
+
+# Confirm all the files have been moved out of to_sort
+oldfolder = os.listdir(dir+to_sort)
+if oldfolder:
+  print("\nIssue - Not all files were successfully moved from to_sort:\n")
+  print(oldfolder)
+else:
+  print("\nAll files sorted and removed from to_sort.")
+      
+    
